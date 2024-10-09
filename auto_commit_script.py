a4dfd08a3b62ac0886750807f4c2b0bcf604f697
jonbrand <jonbranaraujo18@gmail.com>
2024-10-08 21:52:23 -0300

    adding env

--- auto_commit_script.py
+++ auto_commit_script.py
@@ -1,31 +1,39 @@
 import os
 import subprocess
 from datetime import datetime
+from dotenv import load_dotenv
 
-# Diretório onde seus arquivos do Obsidian estão localizados
-obsidian_dir = "/home/jonbrand/Development/obsidian/obsidian"
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
 
-# Navegue até o diretório do Obsidian
-os.chdir(obsidian_dir)
+# Obtém o caminho do diretório do Obsidian a partir das variáveis de ambiente
+obsidian_dir = os.getenv('OBSIDIAN_DIR')
 
-# Verifica se há mudanças não commitadas
-status = subprocess.run(['git', 'status', '--porcelain'],
-                        capture_output=True, text=True)
+# Verifica se o diretório foi carregado corretamente
+if obsidian_dir:
+    # Navegue até o diretório do Obsidian
+    os.chdir(obsidian_dir)
 
-if status.stdout:
-    # Adiciona todas as mudanças ao controle de versão
-    subprocess.run(['git', 'add', '.'])
+    # Verifica se há mudanças não commitadas
+    status = subprocess.run(['git', 'status', '--porcelain'],
+                            capture_output=True, text=True)
 
-    # Cria a mensagem de commit com data e hora
-    commit_date = datetime.now().strftime("%Y/%m/%d %H:%M")
-    commit_message = f"Backup automático: {commit_date}"
+    if status.stdout:
+        # Adiciona todas as mudanças ao controle de versão
+        subprocess.run(['git', 'add', '.'])
 
-    # Realiza o commit
-    subprocess.run(['git', 'commit', '-m', commit_message])
+        # Cria a mensagem de commit com data e hora
+        commit_date = datetime.now().strftime("%Y/%m/%d %H:%M")
+        commit_message = f"Backup automático: {commit_date}"
 
-    # Faz o push das mudanças para o repositório remoto
-    subprocess.run(['git', 'push', 'origin', 'main'])
+        # Realiza o commit
+        subprocess.run(['git', 'commit', '-m', commit_message])
+
+        # Faz o push das mudanças para o repositório remoto
+        subprocess.run(['git', 'push', 'origin', 'main'])
+    else:
+        log_date = datetime.now().strftime("%Y/%m/%d %H:%M")
+        log_message = f"Nenhuma mudança detectada: {log_date}"
+        print(log_message)
 else:
-    log_date = datetime.now().strftime("%Y/%m/%d %H:%M")
-    log_message = f"Nenhuma mudança detectada: {log_date}"
-    print(log_message)
+    print("Erro: Caminho do diretório do Obsidian não encontrado. Verifique o arquivo .env.")
